ad57fdeadb4ca46533a779b2f421689319c3f923
Hatzel.N_99 <125182638+Ngan7699@users.noreply.github.com>
2023-11-26 20:48:56 +0700

    Add files via upload

---
+++ Customer Churn Rate.sql
@@ -0,0 +1,31 @@
+select (total_churn*100)/total_customer as churnrate
+from
+(select SUM(CASE 
+				WHEN status='unactive' then 1
+				else 0
+			end) as total_churn,
+		COUNT(distinct CustomerKey) as total_customer
+from
+(select CustomerKey, status, SUM(SalesAmount) as Total_Sales, count (CustomerKey) as Total_Transaction
+from 
+(select d.CustomerKey,
+	   d.SalesAmount,
+	   case 
+			when d.date_diff <= 180 then 'active'
+			else 'unactive'
+		end as status
+from
+
+(select c.CustomerKey,c.SalesAmount, DATEDIFF(day, max_orderdate, max_date) date_diff
+from
+		(Select a.CustomerKey, a.SalesAmount, a.max_date, b.max_orderdate
+		from
+		(select CustomerKey, SalesAmount, OrderDate,(select MAX(OrderDate) from FactInternetSales) as max_date
+		from FactInternetSales) a
+		left join
+		(select CustomerKey, max (OrderDate) as max_orderdate 
+		from FactInternetSales
+		group by CustomerKey) b
+		on a.CustomerKey = b.CustomerKey) c
+) d) e
+group by CustomerKey, status) f) g
